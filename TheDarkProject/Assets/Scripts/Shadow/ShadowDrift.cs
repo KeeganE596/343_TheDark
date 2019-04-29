ca2b0b68ecc41f8f20bff16d444b6a4258e2fe48
Guest User <Guest@wg401-d-03-06.mddn.vuw.ac.nz>
2019-04-29 12:19:37 +1200

    Added Unity project and basic shadow drift

---
+++ TheDarkProject/Assets/Scripts/Shadow/ShadowDrift.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowDrift : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     	  
+    }
+
+    public void Move() {
+    	Debug.Log("moving");
+    	transform.LookAt(GameObject.FindWithTag("Player").transform.position);
+    	transform.Translate(Vector3.forward * Time.deltaTime*3); 
+    }
+}
---
+++ TheDarkProject/Assets/Scripts/Shadow/ShadowSwitch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowSwitch : MonoBehaviour
+{
+    bool nearPlayer;
+	ShadowDrift shadowDriftScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nearPlayer = false;
+        shadowDriftScript = GetComponent<ShadowDrift>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //Debug.Log(nearPlayer);
+        if(nearPlayer) { 
+        	shadowDriftScript.Move();
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+    	if(other.gameObject.tag == "Player") {
+    		nearPlayer = true;
+    		transform.LookAt(GameObject.FindWithTag("Player").transform.position);
+    	}
+    }
+}

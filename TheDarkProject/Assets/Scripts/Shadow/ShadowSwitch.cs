4644a36ab95e970bc3ade9016e7a2704d4232cb3
Guest User <Guest@wg401-c-06-07.mddn.vuw.ac.nz>
2019-05-03 12:12:01 +1200

    Added shadow random spawn ai

--- TheDarkProject/Assets/Scripts/Shadow/ShadowSwitch.cs
+++ TheDarkProject/Assets/Scripts/Shadow/ShadowSwitch.cs
@@ -6,12 +6,26 @@ public class ShadowSwitch : MonoBehaviour
 {
     bool nearPlayer;
 	ShadowDrift shadowDriftScript;
+    ShadowRandomMove shadowRandomMoveScript;
+    checkInArea checkPlayerInArea;
+    GameObject currentArea;
+    int areaIndex;
+    bool moveRandom;
+
+    float timer;
 
     // Start is called before the first frame update
     void Start()
     {
         nearPlayer = false;
+        areaIndex = 0;
         shadowDriftScript = GetComponent<ShadowDrift>();
+        shadowRandomMoveScript = GetComponent<ShadowRandomMove>();
+        currentArea = shadowRandomMoveScript.currentArea(areaIndex);
+        checkPlayerInArea = currentArea.GetComponent<checkInArea>();
+        moveRandom = true;
+
+        timer = 0;
     }
 
     // Update is called once per frame
@@ -19,15 +33,39 @@ public class ShadowSwitch : MonoBehaviour
     {
         
         //Debug.Log(nearPlayer);
-        if(nearPlayer) { 
+        if(!moveRandom && nearPlayer && checkPlayerInArea.getIfInArea()) { 
         	shadowDriftScript.Move();
+        }
+        else {
+            timer += Time.deltaTime;
+        }
+
+        if(moveRandom && checkPlayerInArea.getIfInArea() && timer > 1) {
+            transform.position = shadowRandomMoveScript.pickArea(areaIndex);
+            timer = 0;
+        }
+
+        if(moveRandom && !nearPlayer && timer > 2) {
+            areaIndex = Random.Range(0, 6);
+            currentArea = shadowRandomMoveScript.currentArea(areaIndex);
+            checkPlayerInArea = currentArea.GetComponent<checkInArea>();
+            transform.position = shadowRandomMoveScript.pickArea(areaIndex);
+
+            timer = 0;
         }
     }
 
     void OnTriggerEnter(Collider other) {
     	if(other.gameObject.tag == "Player") {
     		nearPlayer = true;
+            moveRandom = false;
     		transform.LookAt(GameObject.FindWithTag("Player").transform.position);
     	}
+    }
+    void OnTriggerExit(Collider other) {
+        if(other.gameObject.tag == "Player") {
+            nearPlayer = false;
+            moveRandom = true;
+        }
     }
 }

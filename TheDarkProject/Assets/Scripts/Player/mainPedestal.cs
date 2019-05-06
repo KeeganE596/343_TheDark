2b22bf40e33c1fb6eb5e9ecf3f325c16d4475bc8
Guest User <Guest@wg401-c-07-07.mddn.vuw.ac.nz>
2019-05-06 12:22:20 +1200

    Added player test scene, with item pick up and dream state test

---
+++ TheDarkProject/Assets/Scripts/Player/mainPedestal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainPedestal : MonoBehaviour
+{
+	public GameObject pedestalArtifact;
+	public bool artifactPlaced = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pedestalArtifact.SetActive(false);
+        artifactPlaced = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void makePlaced() {
+    	artifactPlaced = true;
+    	pedestalArtifact.SetActive(true);
+    }
+}

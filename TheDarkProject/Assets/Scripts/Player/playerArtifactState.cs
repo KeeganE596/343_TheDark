63b8c8eae832038dba96c791a9552f0f627ef599
Guest User <Guest@wg401-b-02-06.mddn.vuw.ac.nz>
2019-05-10 11:47:13 +1200

    updated post-processing on dreamstate

--- TheDarkProject/Assets/Scripts/Player/playerArtifactState.cs
+++ TheDarkProject/Assets/Scripts/Player/playerArtifactState.cs
@@ -10,9 +10,12 @@ public class playerArtifactState : MonoBehaviour
 
 	GameObject[] dreamObjects;
 	public GameObject artifactInHand;
-	public GameObject ppVolume;
 
-	
+	PostProcessVolume testVolume;
+	Vignette testVignette;
+	ChromaticAberration testCA;
+	ColorGrading testCG;
+	LensDistortion testLD;
 
     // Start is called before the first frame update
     void Start()
@@ -20,23 +23,34 @@ public class playerArtifactState : MonoBehaviour
         isHoldingArtifact = false;
         dreamObjects = GameObject.FindGameObjectsWithTag("DreamObject");
         artifactInHand.SetActive(false);
-        PostProcessVolume volume = ppVolume.GetComponent<PostProcessVolume>();
     }
 
     // Update is called once per frame
     void Update()
     {
+    	//var sinTest = (Mathf.Sin(Time.realtimeSinceStartup));
+    	//Debug.Log(sinTest);
+
     	if(isHoldingArtifact) {
     		for(int i=0; i<dreamObjects.Length; i++) {
     			dreamObjects[i].SetActive(true);
     		}
-    		ppVolume.SetActive(true);
+
+    		var sinWave = (Mathf.Sin(Time.realtimeSinceStartup*2f));
+    		var cosWave = (Mathf.Cos(Time.realtimeSinceStartup*2f));
+
+    		//testVignette.intensity.value = Mathf.Lerp(0.3f, 0.45f, sinWave);
+    		testCA.intensity.value = Mathf.Lerp(0.3f, 1.0f, sinWave);
+    		testCG.hueShift.value = Mathf.Lerp(-150f, 150f, sinWave);
+    		testLD.intensity.value = Mathf.Lerp(-30f, 30f, sinWave);
+    		testLD.centerX.value = Mathf.Lerp(-0.3f, 0.3f, (Mathf.Sin(Time.realtimeSinceStartup*2.2f)));
+    		testLD.centerY.value = Mathf.Lerp(-0.3f, 0.3f, (Mathf.Cos(Time.realtimeSinceStartup*3f)));
+    		Debug.Log(testLD.centerY.value);
     	}
     	else if(dreamObjects[0].activeSelf) {
     		for(int i=0; i<dreamObjects.Length; i++) {
     			dreamObjects[i].SetActive(false);
     		}
-    		ppVolume.SetActive(false);
     	}
     }
 
@@ -45,9 +59,32 @@ public class playerArtifactState : MonoBehaviour
     	
     	if(isHoldingArtifact) {
     		artifactInHand.SetActive(true);
+    		doEffects();
     	}
     	else {
     		artifactInHand.SetActive(false);
+    		RuntimeUtilities.DestroyVolume(testVolume, true, true);
     	}
+    }
+
+    //enable the post processing volume
+    void doEffects() {
+    	testVignette = ScriptableObject.CreateInstance<Vignette>();
+    	testVignette.enabled.Override(true);
+        testVignette.intensity.Override(0.35f);
+		testCA = ScriptableObject.CreateInstance<ChromaticAberration>();
+		testCA.enabled.Override(true);
+        testCA.intensity.Override(1f);
+		testCG = ScriptableObject.CreateInstance<ColorGrading>();
+		testCG.enabled.Override(true);
+        testCG.hueShift.Override(1f);
+        //testCG.saturation.Override(-25f);
+		testLD = ScriptableObject.CreateInstance<LensDistortion>();
+		testLD.enabled.Override(true);
+        testLD.intensity.Override(1f);
+        testLD.centerX.Override(0f);
+        testLD.centerY.Override(0f);
+
+		testVolume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, testVignette, testCA, testCG, testLD);
     }
 }

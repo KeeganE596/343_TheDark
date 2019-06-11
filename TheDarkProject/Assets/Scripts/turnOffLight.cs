97fa3578f95db07169b087fd3e8a2ee81a9aff83
Keegan <kman596@gmail.com>
2019-06-11 17:43:27 +1200

    Actually adding new assets this time

---
+++ TheDarkProject/Assets/Scripts/turnOffLight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnOffLight : MonoBehaviour
+{
+
+	public GameObject editingLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(editingLight.activeSelf) {
+        	editingLight.SetActive(false);
+        }
+    }
+}

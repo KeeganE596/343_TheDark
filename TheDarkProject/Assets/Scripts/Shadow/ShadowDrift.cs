a0ac70f39d94b077fe824989094e01dcbf8b0f62
Guest User <Guest@wg401-b-01-06.mddn.vuw.ac.nz>
2019-06-14 14:03:25 +1200

    Fixed shadow floating up on chase

--- TheDarkProject/Assets/Scripts/Shadow/ShadowDrift.cs
+++ TheDarkProject/Assets/Scripts/Shadow/ShadowDrift.cs
@@ -15,7 +15,7 @@ public class ShadowDrift : MonoBehaviour
     public void Move() {
     	transform.LookAt(GameObject.FindWithTag("Player").transform.position);
     	transform.Translate(Vector3.forward * Time.deltaTime*4);
-        transform.position += Vector3.up * 0.02f;
+        transform.position += Vector3.up * 0.01f;
         transform.eulerAngles = new Vector3(0f, transform.eulerAngles.y, transform.eulerAngles.z);
         //Debug.Log("normal drift");
     }
@@ -29,7 +29,6 @@ public class ShadowDrift : MonoBehaviour
     public void artifactMove() {
         transform.LookAt(GameObject.FindWithTag("Player").transform.position);
         transform.Translate(Vector3.forward * Time.deltaTime*5f);
-        transform.position += Vector3.up * 0.02f;
         transform.eulerAngles = new Vector3(0f, transform.eulerAngles.y, transform.eulerAngles.z);
         //Debug.Log("finding");
     }
